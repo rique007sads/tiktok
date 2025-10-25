52eca6dc572bc3e7c1486891032b3bf8cdc24adb
Lucas <01lucasgsoares01@gmail.com>
2025-10-25 01:54:17 +0000

    /* Este código deve ser adicionado ao seu arquivo CSS principal */
    /* *

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
+        body: ['Montserrat', 'sans-serif'],
         headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
