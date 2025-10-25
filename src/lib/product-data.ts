adc57b78a8b32c614d65cc2e5df24ad1c945aab3
Lucas <01lucasgsoares01@gmail.com>
2025-10-25 02:54:32 +0000

    https://i.postimg.cc/dtRXTFwr/Cn-P-24102025-235333.png substitua a image

--- src/lib/product-data.ts
+++ src/lib/product-data.ts
@@ -95,7 +95,8 @@ export const productData: Product = {
               nota: 4,
               data: "2023-10-27T10:15:00Z",
               texto: "Gostei bastante, achei que era mais lento mas a velocidade me surpreendeu, no manual diz que vai ate 25 mas claramente da pra ver a velocidade almentando quando chega no limite de 25, deve chegar nos 30, a bateria dura até que bem, recarregamos a cada 3 dias.",
-              fotoUrlComentario: "https://i.postimg.cc/43WHYDHF/Cn-P-24102025-233352.png"
+              fotoUrlComentario: "https://i.postimg.cc/43WHYDHF/Cn-P-24102025-233352.png",
+              fotoUrlAutor: "https://i.postimg.cc/dtRXTFwr/Cn-P-24102025-235333.png"
           },
           "comentarioId_PR": {
               autor: "Pedro Raul",
