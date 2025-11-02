23160ce9e18baa36761306d60c630cd6479554eb
Henrique De paula <henrique.negrao23@gmail.com>
2025-11-02 22:45:17 +0000

    coloque essa imagem no comentario da fernanda  https://i.postimg.cc/02hn

--- src/lib/product-data.ts
+++ src/lib/product-data.ts
@@ -123,7 +123,7 @@ export const productData: Product = {
           data: "2023-11-01T14:10:00Z",
           texto: "Sensacional! Uso o Meta Quest 3s pra treinar boxe virtual e até pra relaxar assistindo vídeos. É leve, confortável e o som 3D é incrível. Recomendo pra quem quer tecnologia e diversão em um só produto! 💪🎧",
           fotoUrlComentario: null,
-          fotoUrlAutor: "https://i.postimg.cc/43w8ZDrd/images.jpg"
+          fotoUrlAutor: "https://i.postimg.cc/02hnbckT/A9994eee6ed9d4f758035b4693e6b2e5bq-jpeg-960x960q75-jpeg.avif"
         }
       ]
     }
@@ -229,6 +229,7 @@ export const imageMap: Record<string, ProductImage> = imagePlaceholders.reduce(
     
 
     
+
 
 
 
